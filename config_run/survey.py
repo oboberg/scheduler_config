6e0eb60918d4891e1ee77dd64edc9ba4b47c0661
oboberg <boberg37@gmail.com>
2018-09-06 12:33:42 -0700

    add modified nes config

--- config_run/survey.py
+++ config_run/survey.py
@@ -13,6 +13,3 @@ config.start_date='2022-10-01'
 
 # The fractional duration (units=years) of the survey.
 config.duration=10.0
-
-#Do not include NES in general_propsals list
-config.general_proposals=['GalacticPlane', 'SouthCelestialPole', 'WideFastDeep']
